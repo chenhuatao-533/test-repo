771d655d408f5737339879e80271f6af65a3736f
chenhuatao-533 <chenhuatao520533@163.com>
2025-06-19 15:16:02 +0800

    这是一次冲突解决

--- test.c
+++ test.c
@@ -1,9 +1,10 @@
-//这是用来模拟远程仓库推送自己提交时候，其他人也正在提交，解决冲突。
+// 这是用来模拟远程仓库推送自己提交时候，其他人也正在提交，解决冲突。
 #include <stdio.h>
 
 int main()
 {
-    printf("This is message for master.\n");  //这条代码是在master分支上写的。
-    printf("This is message for other.\n");  //这是在other分支上写的。
+    printf("This is message for master.\n"); // 这条代码是在master分支上写的。
+    printf("This is message for other.\n");  // 这是在other分支上写的。
+    printf("This is message for me.\n");     // 这是在me分支上写的。
     return 0;
 }
